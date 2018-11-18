56c93215f1be7480d902f60cc9760946450bef2b
Oleg Zudov <tocsharp@gmail.com>
2018-11-18 21:41:31 +0300

    WebDriverTimeoutException added

---
+++ AsyncWebDriver/Exceptions/WebDriverTimeoutException.cs
@@ -0,0 +1,58 @@
+// Copyright (c) Oleg Zudov. All Rights Reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// This file is based on or incorporates material from the project Selenium, licensed under the Apache License, Version 2.0. More info in THIRD-PARTY-NOTICES file.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Zu.AsyncWebDriver
+{
+    /// <summary>
+    /// Represents exceptions that are thrown when an error occurs during actions.
+    /// </summary>
+    [Serializable]
+    public class WebDriverTimeoutException : WebDriverException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebDriverTimeoutException"/> class.
+        /// </summary>
+        public WebDriverTimeoutException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebDriverTimeoutException"/> class with
+        /// a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public WebDriverTimeoutException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebDriverTimeoutException"/> class with
+        /// a specified error message and a reference to the inner exception that is the
+        /// cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception,
+        /// or <see langword="null"/> if no inner exception is specified.</param>
+        public WebDriverTimeoutException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebDriverTimeoutException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized
+        /// object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual
+        /// information about the source or destination.</param>
+        protected WebDriverTimeoutException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
